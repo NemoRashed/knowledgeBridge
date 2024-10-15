1446657a7678d83cbb3e04a7e8eaddfbc121f879
Lenovo <nemorashed1212@gmail.com>
2024-10-14 18:37:12 -0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-white flex justify-center items-center text-3xl">
+      Bismillah
+    </div>
+  );
+}
